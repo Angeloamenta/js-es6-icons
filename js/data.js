b2255b694ff8bf7b529876b0787b13aba7b36e25
Angeloamenta <angeloamenta7@gmail.com>
2021-12-13 16:01:22 +0100

    selection ok

--- js/data.js
+++ js/data.js
@@ -117,17 +117,85 @@ const boxes = [
 // Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell'icona e l'icona stessa.
 
 const main = document.querySelector(".main");
-boxes.forEach((element) => {
-    main.innerHTML += `
-        <div class="box">
-		<i class="fas fa-${element.name}" style="color:${element.color}"></i>
-		${element.name}
-        </div>
-    `;
-});
+// boxes.forEach((element) => {
+//     main.innerHTML += `
+//         <div class="box">
+// 		<i class="fas fa-${element.name}" style="color:${element.color}"></i>
+// 		${element.name}
+//         </div>
+//     `;
+// });
 
 // Milestone 2
 // Ciascuna icona ha una proprietà "color": utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
+const filter = document.getElementById("filter");
+ const all = document.querySelector(".all");
+ const animal = document.querySelector(".animal");
+ const vegetable = document.querySelector(".vegetable");
+ const user = document.querySelector(".user");
+
+ filter.addEventListener('change', function () {
+	 console.log(filter.value);
+	  if (filter.value == animal.value) {
+		main.innerHTML = " ";
+
+		let colorOrange = boxes.filter((element) => {
+			return element.color == "orange";
+		});
+
+		colorOrange.forEach((element) => {
+			main.innerHTML += `
+				<div class="box">
+				<i class="fas fa-${element.name}" style="color:${element.color}"></i>
+				${element.name}
+				</div>
+			`;
+		});
+	  } else if (filter.value == vegetable.value) {
+		main.innerHTML = " ";
+
+		let colorGreen = boxes.filter((element) => {
+			return element.color == "green";
+		});
+
+		colorGreen.forEach((element) => {
+			main.innerHTML += `
+				<div class="box">
+				<i class="fas fa-${element.name}" style="color:${element.color}"></i>
+				${element.name}
+				</div>
+			`;
+		});
+	  } else if (filter.value == user.value) {
+		main.innerHTML = " ";
+
+		let colorBlue = boxes.filter((element) => {
+			return element.color == "blue";
+		});
+
+		colorBlue.forEach((element) => {
+			main.innerHTML += `
+				<div class="box">
+				<i class="fas fa-${element.name}" style="color:${element.color}"></i>
+				${element.name}
+				</div>
+			`;
+		});
+	  } else {
+		main.innerHTML = " ";
+
+		   boxes.forEach((element) => {
+			main.innerHTML += `
+         	<div class="box">
+			<i class="fas fa-${element.name}" style="color:${element.color}"></i>
+			${element.name}
+        	</div>
+     	`;
+	});
+	  }
+	
+  });
+
 
 
 
